d77bdeb5c29b30902e273992383b63d4bc56a30b
Sefa Çiçekli <46853577+sefacicekli@users.noreply.github.com>
2021-12-10 15:11:07 +0300

    Update client.js

--- plugins/client.js
+++ plugins/client.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 const supabase = createClient(
-    "https://yiawkwultwutcmhoqjrd.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDczNjQ5OSwiZXhwIjoxOTM2MzEyNDk5fQ.0xN6nlps7cHy-3F4hnf7p07hT5WqIQHFmYg9hkyn04g"
+    "yourProjectUrl",
+    "ApiKey"
 )
 export default (_, inject) => {
     inject('supabase', supabase)
